761266284465e81dc3ce32ef4c3dcf716b5566f2
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2023-09-22 20:37:30 +0530

    Create First_and_last_occurrences_of_x_Solution.cpp

---
+++ First_and_last_occurrences_of_x_Solution.cpp
@@ -0,0 +1,83 @@
+****************************************************************************************  Question  ****************************************************************************************************************
+Given a sorted array arr containing n elements with possibly duplicate is to find indexes of first elements, the task is to find the first and last occurrences of an element x in the given array.
+Note: If the number x is not found in the array then return both the indices as -1.
+
+
+Example 1:
+
+Input:
+n=9, x=5
+arr[] = { 1, 3, 5, 5, 5, 5, 67, 123, 125 }
+Output:  
+2 5
+Explanation: 
+First occurrence of 5 is at index 2 and last occurrence of 5 is at index 5. 
+Example 2:
+
+Input:
+n=9, x=7
+arr[] = { 1, 3, 5, 5, 5, 5, 7, 123, 125 }
+Output:  
+6 6
+Explanation: 
+First and last occurrence of 7 is at index 6.
+Your Task:
+Since, this is a function problem. You don't need to take any input, as it is already accomplished by the driver code. You just need to complete the function find() that takes array arr, integer n and integer x as parameters and returns the required answer.
+
+Expected Time Complexity: O(logN)
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ N ≤ 106
+1 ≤ arr[i],x ≤ 109
+
+
+
+
+****************************************************************************************  Solution  ****************************************************************************************************************
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+   vector<int> find(int ar[], int n , int x ) {
+        int l{}, r{n - 1}, fst{-1}, lst{-1};
+        
+        while(l <= r) {
+            if(ar[l] != x) ++l;
+            if(ar[r] != x) --r;
+            if(ar[l] == x and ar[r] == x) return {l, r};
+        }
+        
+        return {fst, lst};
+}
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        Solution ob;
+        ans=ob.find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
+
